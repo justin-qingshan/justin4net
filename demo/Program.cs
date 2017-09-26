9d1a1894b866309cf97f6d44d374631f486f7ecc
Justin-Qingshan <justin.qingshan@gmail.com>
2017-09-26 10:11:28 +0800

    add project of demo.

---
+++ demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\GIT\just4net1\just4net\io";
+            string name = Path.GetFileName(path);
+            string nameWithoutEx = Path.GetFileNameWithoutExtension(path);
+            string dirName = Path.GetDirectoryName(path);
+
+            Console.WriteLine(path);
+            Console.WriteLine();
+            Console.WriteLine(name);
+            Console.WriteLine(nameWithoutEx);
+            Console.WriteLine(dirName);
+            Console.ReadKey();
+        }
+
+        
+    }
+}
