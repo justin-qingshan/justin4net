fed8e1023223d528a94a1e3ee9656dd773e7adb1
Justin-Qingshan <justin.qingshan@gmail.com>
2017-09-28 16:22:03 +0800

    update readme.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # justin4net
+
+## just4net
+
+The main tool.
+
+
+### util
+
+General tools.
+
+### io
+
+Include tools of io, such as FileUtil, DirectoryUtil and so on.
+
+
+
+## demo
+The demo of tools using.
--- demo/Program.cs
+++ demo/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.IO;
+using just4net.io;
 
 namespace demo
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            TestMD5();
+            Console.ReadKey();
+        }
+
+
+
+        static void TestPath()
         {
             string path = @"D:\GIT\just4net1\just4net\io";
             string name = Path.GetFileName(path);
@@ -17,9 +26,18 @@ namespace demo
             Console.WriteLine(name);
             Console.WriteLine(nameWithoutEx);
             Console.WriteLine(dirName);
-            Console.ReadKey();
+        }
+        
+        static void TestMD5()
+        {
+            string path = @"D:\GIT\justin4net\README.md";
+            string md5 = FileUtil.MD5(path);
+            string sha1 = FileUtil.SHA1(path);
+
+            Console.WriteLine(path);
+            Console.WriteLine("MD5: {0}", md5);
+            Console.WriteLine("SHA1: {0}", sha1);
         }
 
-        
     }
 }
